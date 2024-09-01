f51af9b18d32888765d164be4b135a3f88d5948d
ChathuminiIsh <chathumini.ish@gmail.com>
2024-09-01 19:39:41 +0530

    initial commit

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,36 @@
+package net.javaguides.springboot;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee=new Employee();
+		employee.setFirstName("Chathumini");
+		employee.setLastName("Ishara");
+		employee.setEmailId("chathumini.ish@gmail.com");
+		employeeRepository.save(employee);
+
+		Employee employee1=new Employee();
+		employee1.setFirstName("Kamal");
+		employee1.setLastName("Perera");
+		employee1.setEmailId("kamal@gmail.com");
+		employeeRepository.save(employee1);
+
+
+
+	}
+}
